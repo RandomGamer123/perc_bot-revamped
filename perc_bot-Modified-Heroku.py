6c0491b80f3e2f09bd33d72da033917a81dc9c6d
RandomGamer123 <31096837+RandomGamer123@users.noreply.github.com>
2019-09-30 20:45:25 -0400

    Test

--- perc_bot-Modified-Heroku.py
+++ perc_bot-Modified-Heroku.py
@@ -1200,6 +1200,7 @@ async def on_message(message):
                 nerd = await client.get_user_info('210285266814894081')
                 await client.send_message(nerd, '{} has bought {}.'.format(message.author.name,item))
                 random = await client.get_user_info('156390113654341632')
+                await asyncio.sleep(1)
                 print(str(random.id))
                 await client.send_message(random, '{} has bought {}.'.format(message.author.name,item))
             elif success==4:
